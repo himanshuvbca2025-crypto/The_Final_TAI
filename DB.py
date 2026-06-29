2aade16f8b500ee6c52894e3e69bec461d8ba39c
Himanshu Verma <himanshuv.bca2025@ssism.org>
2026-06-29 21:16:08 +0530

    Fix database configuration

--- DB.py
+++ DB.py
@@ -10,14 +10,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST = os.getenv("MYSQLHOST", "localhost") or os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("MYSQLUSER", "root")or os.getenv("DB_USER","root")
-DB_PASSWORD = os.getenv("MYSQLPASSWORD", "") or os.getenv("DB_PASSWORD", "")
-DB_NAME = os.getenv("MYSQLDATABASE") or os.getenv("DB_NAME", "trinetra_ai")
-DB_PORT = int(os.getenv("MYSQLPORT", os.getenv("DB_PORT", "3306")))
-print("MYSQLDATABASE =", os.getenv("MYSQLDATABASE"))
-print("MYSQL_DATABASE =", os.getenv("MYSQL_DATABASE"))
-print("DB_NAME =", DB_NAME)
+
+DB_HOST = os.getenv("MYSQLHOST", "localhost")
+DB_USER = os.getenv("MYSQLUSER", "root")
+DB_PASSWORD = os.getenv("MYSQLPASSWORD", "")
+DB_NAME = os.getenv("MYSQLDATABASE") or os.getenv("MYSQL_DATABASE", "trinetra_ai")
+DB_PORT = int(os.getenv("MYSQLPORT") or 3306)
+
+print("HOST =", DB_HOST)
+print("USER =", DB_USER)
+print("DB =", DB_NAME)
 
 # ==========================================
 # DATABASE CONNECTION
